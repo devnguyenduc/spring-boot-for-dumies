24fc774ad604b4b86f98c4ea20e4f378d2e13104
Nam Dam <dthnam90@gmail.com>
2019-11-03 14:54:29 +0700

    Kickoff the project

---
+++ src/main/groovy/com/a/o/shield/springbootfordummies/SpringBootForDummiesApplication.groovy
@@ -0,0 +1,13 @@
+package com.a.o.shield.springbootfordummies
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootForDummiesApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringBootForDummiesApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/a/o/shield/springbootfordummies/SpringBootForDummiesApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.a.o.shield.springbootfordummies
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootForDummiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
